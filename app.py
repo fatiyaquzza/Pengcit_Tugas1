6f63651a5279a141159fe3ab57d3c905e3d0d51d
Ayuauliaaa <ayu_au@mhs.unsyiah.ac.id>
2023-09-13 16:48:53 +0700

    feat : membuat halaman blur

--- app.py
+++ app.py
@@ -78,8 +78,32 @@ def upload_file():
     
     return render_template('index.html')
 
-@app.route('/blur')
-def blur():
+
+@app.route('/blur', methods=['GET', 'POST'])
+def blurWajah():
+    if request.method == 'POST':
+            file = request.files['img']
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD'], filename))
+            img_path = os.path.join(app.config['UPLOAD'], filename)
+
+            # Membaca gambar dengan OpenCV
+            img = cv2.imread(img_path)
+
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+
+            for (x, y, w, h) in faces:
+                face_roi = img[y:y + h, x:x + w]
+                face_roi = cv2.GaussianBlur(face_roi, (23, 23), 30)
+                img[y:y + face_roi.shape[0], x:x + face_roi.shape[1]] = face_roi
+
+            # Simpan foto hasilnya
+            output_filename = 'hasil_blur.jpg'
+            cv2.imwrite(output_filename, img)
+            
+            return render_template('blur.html', img=img_path, result_filename=output_filename)
     # show the form, it wasn't submitted
     return render_template('blur.html')
 
